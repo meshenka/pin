cb55d2b6b3cc991edfaf1ab3c7fb70d93da2d9df
Sylvain Gogel <sylvain.gogel@gmail.com>
2021-03-13 19:58:47 +0100

    refactor(pin): use int instead of strings

--- pin.go
+++ pin.go
@@ -1,9 +1,35 @@
 package pin
 
 import (
+	"fmt"
 	"math/rand"
-	"strings"
 	"time"
+)
+
+var (
+	nextMap = map[int]int{
+		0: 1,
+		1: 2,
+		2: 3,
+		3: 4,
+		4: 5,
+		6: 7,
+		7: 8,
+		8: 9,
+		9: 0,
+	}
+	previousMap = map[int]int{
+		0: 9,
+		1: 0,
+		2: 1,
+		3: 2,
+		4: 3,
+		5: 4,
+		6: 5,
+		7: 6,
+		8: 7,
+		9: 8,
+	}
 )
 
 // Generate security rules
@@ -14,77 +40,49 @@ import (
 // cannot be the same, the previous or the next
 func Generate() string {
 
-	var pin []string
 	var current = first()
+	pin := []int{current}
 
-	pin = append(pin, current)
+	for i := 0; i < 3; i++ {
+		current = chooseNext(current)
+		pin = append(pin, current)
+	}
 
-	current = chooseNext(current)
-	pin = append(pin, current)
-
-	current = chooseNext(current)
-	pin = append(pin, current)
-
-	current = chooseNext(current)
-	pin = append(pin, current)
-
-	return strings.Join(pin, "")
+	//return strings.Join(pin, "")
+	return fmt.Sprintf("%v", pin)
 }
 
-func next(s string) string {
-	nextMap := make(map[string]string)
-
-	nextMap["0"] = "1"
-	nextMap["1"] = "2"
-	nextMap["2"] = "3"
-	nextMap["3"] = "4"
-	nextMap["4"] = "5"
-	nextMap["5"] = "6"
-	nextMap["6"] = "7"
-	nextMap["7"] = "8"
-	nextMap["8"] = "9"
-	nextMap["9"] = "0"
-
+func next(s int) int {
 	return nextMap[s]
 }
 
-func previous(s string) string {
-	nextMap := make(map[string]string)
-
-	nextMap["0"] = "9"
-	nextMap["1"] = "0"
-	nextMap["2"] = "1"
-	nextMap["3"] = "2"
-	nextMap["4"] = "3"
-	nextMap["5"] = "4"
-	nextMap["6"] = "5"
-	nextMap["7"] = "6"
-	nextMap["8"] = "7"
-	nextMap["9"] = "8"
-
-	return nextMap[s]
+func previous(s int) int {
+	return previousMap[s]
 }
 
 var seededRand *rand.Rand = rand.New(
 	rand.NewSource(time.Now().UnixNano()))
 
-func inCharset(charset string) string {
-	b := make([]byte, 1)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
-	}
-	return string(b)
+func inCharset(cs []int) int {
+	return cs[seededRand.Intn(len(cs))]
 }
 
-func first() string {
-	return inCharset("1234567890")
-}
-
-func chooseNext(current string) string {
-	charset := "1234567890"
-	//remove current next and previous
-	charset = strings.Replace(charset, current, "", -1)
-	charset = strings.Replace(charset, next(current), "", -1)
-	charset = strings.Replace(charset, previous(current), "", -1)
+func first() int {
 	return inCharset(charset)
+}
+
+var charset = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+func chooseNext(current int) int {
+
+	_charset := []int{}
+	next := next(current)
+	prev := previous(current)
+	for v := range charset {
+		if v != current && v != next && v != prev {
+			_charset = append(_charset, v)
+		}
+	}
+
+	return inCharset(_charset)
 }
